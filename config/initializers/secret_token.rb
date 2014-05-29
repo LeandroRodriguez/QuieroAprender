2495b03eae7f86e2ba55f27262389a4eeddeb4f9
lrodriguez <leandro.rodriguez@prosoftwarefactory.com.ar>
2014-05-29 03:06:09 -0300

    Project kick-off

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuieroAprender::Application.config.secret_key_base = '0f521b8c03dd67fe37d5b01ece0ecf34fde7ba411fd5af9ac03f616703842d60192638783121d2d20f565f6fc37b44ca0c9c3ee33bdad8891a6ceceeded323ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuieroAprender::Application.config.session_store :cookie_store, key: '_quiero_aprender_session'

af47de4ea24c8fa127c9f5b26486e5a3b51f07c2
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-06-01 19:14:40 -0300

    Mapa funcionando. Autocomplete de direcciones funcionando.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	def index
 		# Aca deberian estar los cursos que matchean lo que el user busca
 		@courses = Course.all
-		@hash = Gmaps4rails.build_markers(@users) do |course, marker|
+		@hash = Gmaps4rails.build_markers(@courses) do |course, marker|
 			marker.lat course.latitude
 			marker.lng course.longitude
 		end

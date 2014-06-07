92eb362a4d1d2378646d09eff7a341ef5d05dd15
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-06-07 11:30:54 -0300

    Agrego archivo con informacion default de la base de datos asi todos la pueden probar.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(:name => "Musica")
+Category.create(:name => "Idioma")
+Category.create(:name => "Baile")
+Subcategory.create(:name => "Guitarra", :category_id => 1)
+Subcategory.create(:name => "Piano", :category_id => 1)
+Subcategory.create(:name => "Bateria", :category_id => 1)
+
+Subcategory.create(:name => "Ingles", :category_id => 2)
+Subcategory.create(:name => "Frances", :category_id => 2)
+Subcategory.create(:name => "Aleman", :category_id => 2)
+Subcategory.create(:name => "Italiano", :category_id => 2)
+
+
+Subcategory.create(:name => "Salsa", :category_id => 3)
+Subcategory.create(:name => "Tango", :category_id => 3)
+Subcategory.create(:name => "Capoeira", :category_id => 3)

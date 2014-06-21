0d085744902f6b39b12d5af013160a7274361f71
Florencia Bosch <florb128@gmail.com>
2014-06-21 12:47:12 -0300

    Agrego schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608155120) do
+ActiveRecord::Schema.define(version: 20140620230207) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20140608155120) do
 
   add_index "courses", ["subcategory_id"], name: "index_courses_on_subcategory_id"
 
+  create_table "courses_tags", id: false, force: true do |t|
+    t.integer "course_id"
+    t.integer "tag_id"
+  end
+
   create_table "plans", force: true do |t|
     t.string   "name"
     t.string   "type"
@@ -42,7 +47,13 @@ ActiveRecord::Schema.define(version: 20140608155120) do
   end
 
   create_table "subcategories", force: true do |t|
+    t.string   "name"
     t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"

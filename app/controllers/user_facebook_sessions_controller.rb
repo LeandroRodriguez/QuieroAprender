18d2169463c6ebfaa9ed3ef1af0384a2e8338279
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2014-06-28 21:19:38 -0300

    Ajusto el panel de busqueda avanzada. Reordeno el archivo de routes. elimino archivos viejos.

--- app/controllers/user_facebook_sessions_controller.rb
+++ app/controllers/user_facebook_sessions_controller.rb
@@ -5,12 +5,7 @@ class UserFacebookSessionsController < ApplicationController
 
   end
 
-  #def login
-    
-   # render 'user_sessions/login'
-  #end
-
-  def facebook_login
+  def login
     if params[:code]
       # acknowledge code and get access token from FB
       session[:access_token] = session[:oauth].get_access_token(params[:code])
@@ -24,7 +19,7 @@ class UserFacebookSessionsController < ApplicationController
     rescue Exception=>ex
       puts ex.message
       #if user is not logged in and an exception is caught, redirect to the page where logging in is requested
-      redirect_to '/user_facebook_sessions/facebook_authorization' and return
+      redirect_to :fb_authorization and return
     end
 
     respond_to do |format|
@@ -32,7 +27,7 @@ class UserFacebookSessionsController < ApplicationController
     end
   end
 
-  def facebook_authorization
+  def authorization
     session[:oauth] = Koala::Facebook::OAuth.new(Facebook::APP_ID, Facebook::SECRET, Facebook::SITE_URL)
     @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream publish_stream")
 

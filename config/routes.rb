54e4ca0f2b7baf98542c830a28a1742cf57c9aac
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2014-07-22 09:27:03 -0300

    Agrego una primitiva pantalla para mirar el "detalle" del plan

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ QuieroAprender::Application.routes.draw do
   
   resources :plans
     match 'plans' => "plans#index", :as => :plans_index, via: [:get, :post]
+    match 'plans/:id' => "plans#show", :as => :plans_show, via: [:get, :post]
   
   resources :teachers
     match 'home' => "welcome#index", :as => :home, via: [:get, :post]

3c8e0de64541aa8cbe90630d1d44abedba574d16
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2014-07-28 23:51:12 -0300

    Preparo el método para obtener la url de la imagen de profile.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def get_profile_picture_for_user(user)
+    puts "User: #{@user}"
+    #if user.facebook_id then
+     # puts "http://graph.facebook.com/#{@user.facebook_id}/picture"
+      #return "http://graph.facebook.com/#{@user.facebook_id}/picture"
+    #else
+      return "http://sndt.ac.in/images/courses/courses-imgs.jpg"
+    #end
+  end
 end

f31c34f80aca2e4a7c6c910fb73718a63c513d04
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2014-08-02 14:56:22 -0300

    Mejoro la página de detalle del plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,10 +1,36 @@
 class Plan < ActiveRecord::Base
+	
+	def type_title
+		return ""
+	end
+
+	def general_description
+		return ""
+	end
+
 end
 
 class VisitsPlan < Plan
-  
+
+	def type_title
+		return "Plan por cantidad de visitas"
+	end
+
+	def general_description
+		return "Un plan ideal que le brinda la posibilidad de ofrecer todos los datos de tus cursos para lograr un mayor alcance."
+	end
+
 end
 
 class RegistrationsPlan < Plan
-  
+
+	def type_title
+		return "Plan por cantidad de inscripciones"
+	end
+
+	def general_description
+		return "Un plan recomendado para aquellos que desean lograr un mayor alcance a cambio de un mayor control de sus datos.
+El vitante del curso no podrá ver los datos sensibles como la dirección exacta del lugar del dictado del curso hasta el momento de inscripción."
+	end
+
 end
---
+++ app/views/plans/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @plan, :id, :name, :type, :price, :active, :created_at, :updated_at

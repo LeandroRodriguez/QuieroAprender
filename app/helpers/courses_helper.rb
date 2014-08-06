09b6884653d76e849fbfbd8435bd293f43a0360b
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2014-08-06 01:14:22 -0300

    Agrego el random machine para cantidad de amigos que hicieron el curso

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,17 @@
 module CoursesHelper
+
+	def is_friends_have_taken_a_course(course)
+		friends_count = rand(4);
+	end
+
+	def string_count_of_friends_taken_a_course
+		friends_count = 1 + rand(3)
+		if friends_count == 1 then
+			return "Un amigo ha realizado este curso"
+		end
+		if friends_count > 1 then
+			return "#{friends_count} amigos han realizado este curso"
+		end
+	end
+
 end
